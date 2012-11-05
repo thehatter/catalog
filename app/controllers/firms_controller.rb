389aabc3863a59729522346c3602890f97e2dd29
thehatter <andrewlutenko@gmail.com>
2012-11-05 14:24:17 +0300

    after adding cool select for category in firm_new

--- app/controllers/firms_controller.rb
+++ app/controllers/firms_controller.rb
@@ -20,6 +20,7 @@ class FirmsController < ApplicationController
 
   def new
     @firm = Firm.new
+    @categories = ancestry_options(Category.scoped.arrange(:order => 'name')) {|i| "#{'-' * i.depth} #{i.name}" }
   end
 
   def edit
@@ -68,5 +69,17 @@ class FirmsController < ApplicationController
         flash[:error] = "current_user is not fk admin!" if !current_user.admin?
       end
     end
+
+  def ancestry_options(items, &block)
+    return ancestry_options(items){ |i| "#{'-' * i.depth} #{i.name}" } unless block_given?
+
+    result = []
+    items.map do |item, sub_items|
+      result << [yield(item), item.id]
+      #this is a recursive call:
+      result += ancestry_options(sub_items, &block)
+    end
+    result
+  end
     
 end
