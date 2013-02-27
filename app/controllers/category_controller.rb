fd0e72ea0532febd0cd3d8a384fd0d65bf19f598
thehatter <andrewlutenko@gmail.com>
2013-02-27 21:30:20 +0300

    remove some old files

--- app/controllers/category_controller.rb
+++
@@ -1,9 +0,0 @@
-class CategoryController < ApplicationController
-  before_filter :authenticate_user!, :only => [:new, :create, :destroy, :edit , :update]
-  before_filter :is_admin?, :only => [:destroy, :edit , :update, :index]
-
-  def index
-    @category = Category.all
-  end
-
-end
--- db/migrate/20130125142025_add_firm_id_to_phones.rb
+++
@@ -1,7 +0,0 @@
-class AddFirmIdToPhones < ActiveRecord::Migration
-  def change
-    add_column :phones, :firm_id, :string
-
-    add_index :phones, :firm_id
-  end
-end
